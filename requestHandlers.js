1eb93ed947ccc83373c8fdcca2153206ff38f84b
Nate Koechley <nate@koechley.com>
2011-05-01 15:59:30 -0700

    reponding request handlers with non-blocking operations

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,10 +1,22 @@
-function start() {
+var exec = require("child_process").exec;
+
+function start(response) {
   console.log("Request handler 'start' was called.");
+
+  exec("ls -lah", function (error, stdout, stderr) {
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write(stdout);
+    response.end();
+  });
 }
 
-function upload() {
+function upload(response) {
   console.log("Request handler 'upload' was called.");
+  response.writeHead(200, {"Content-Type": "text/html"});
+  response.write("Hello Upload");
+  response.end();
 }
 
 exports.start = start;
 exports.upload = upload;
+
